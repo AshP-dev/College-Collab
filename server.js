48c15ddf17aeeafcd1f5d8b4a3a7113240b143fd
AshP-dev <ashwatthap@gmail.com>
2021-07-30 13:32:31 +0530

    Packages and Dependencies Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => res.send('API running'));
+//"re.send" sends data to the browser
+
+const PORT = process.env.PORT || 4000; 
+
+/*
+looks for an environment variable called "PORT"; helps when deploying with Heroku and supplies the port number.If there is no environment variable set, it will run at local port 5000.
+*/
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+
+// callback condition in case the Port connects with local or cloud port 
