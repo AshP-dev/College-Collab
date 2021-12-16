1c7e7aa390564428ea848f7f8643ac0ff6dd6388
AshP-dev <ashwatthap@gmail.com>
2021-12-16 13:49:39 +0530

    Done with Redux Setup and Alerts. Started work on React User Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+//Redux
+import { Provider } from "react-redux"; //connects react and redux
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
 import "./App.css";
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Alert />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Init Middleware (to make req.body to work in User.js)
-app.use(express.json({extended :false }));
+app.use(express.json({ extended: false }));
 
 // Connecting to the Database
 connectDB();
 
-app.get('/',(req,res) => res.send('API running'));
+app.get("/", (req, res) => res.send("API running"));
 //"re.send" sends data to the browser
 
 //Define Routes
-app.use('/api/users', require('./config/routes/api/users')); 
-//here, "/api/users" is the endpoint that pertains to the mentioned user's files 
-app.use('/api/auth', require('./config/routes/api/auth')); 
-app.use('/api/profile', require('./config/routes/api/profile')); 
-app.use('/api/posts', require('./config/routes/api/posts')); 
+app.use("/api/users", require("./config/routes/api/users"));
+//here, "/api/users" is the endpoint that pertains to the mentioned user's files
+app.use("/api/auth", require("./config/routes/api/auth"));
+app.use("/api/profile", require("./config/routes/api/profile"));
+app.use("/api/posts", require("./config/routes/api/posts"));
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
 
 /*
 looks for an environment variable called "PORT"; helps when deploying with Heroku and supplies the port number.If there is no environment variable set, it will run at local port 5000.
@@ -28,4 +27,4 @@ looks for an environment variable called "PORT"; helps when deploying with Herok
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
 
-// callback condition in case the Port connects with local or cloud port 
+// callback condition in case the Port connects with local or cloud port
